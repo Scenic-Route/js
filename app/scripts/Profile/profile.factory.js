03cca03bed5fa64962c69758aa7cabfe6cc53e72
Dane Worsham <zenki14@gmail.com>
2015-03-17 17:13:28 -0400

    started Vars on Routes Factory

--- app/scripts/Profile/profile.factory.js
+++ app/scripts/Profile/profile.factory.js
@@ -10,13 +10,7 @@
     var user = UserFactory.user;
 
 
-    var getAllJobs = function(res){
-      return $http.get(HEROKU.URL + 'users/listings', {
-          headers: HEROKU.CONFIG.headers,
-          // cache: true
-      });
-
-    };
+  
 
     var getProfile = function (id){
       return $http.get(HEROKU.URL + 'profiles/' + id, 
@@ -31,30 +25,7 @@
         );
     };
 
-    // var editProfile = function(listObj){
-    //   return $http.post(HEROKU.URL + 'users/listings', listObj, HEROKU.CONFIG);
-    //   // headers: HEROKU.CONFIG.headers
-    // };
-
-
-    // var deleteList = function (id){
-    //   return $http.delete(HEROKU.URL + 'users/listings/' + id, HEROKU.CONFIG);
-    // };
-
-    // var preInt = function (id) {
-    //   return $http.post(HEROKU.URL + $interpolate('users/listings/:id/interviews'), 
-    //     {headers: HEROKU.CONFIG.headers,}
-    //     );
-
-    // };
-
-    // var addInterview = function (id, listObj){
-    //   return $http.post(HEROKU.URL + 'users/listings/' + id + '/interviews', listObj, {header: HEROKU.CONFIG.headers});
-    // };
-
-    // var addPostInterview = function (){
-    //   return $http.post(HEROKU.URL)
-    // }
+   
 
 
 
--- app/scripts/Routes/routes.factory.js
+++ app/scripts/Routes/routes.factory.js
@@ -7,8 +7,10 @@
 	.factory('RoutesFactory', ['$http', 'HEROKU', '$location', '$routeParams', 'uiGmapGoogleMapApi',
 		function ($http, HEROKU, $location, $scope, $routeParams){
 
-			var getOneRoute = function (){
+			var getOneRoute = function (id){
 				console.log('Getting One Route!')
+				return $http.get(HEROKU.URL + 'routes/' + id, HEROKU.CONFIG,
+					)
 			}
 
 			var getAllRoutes = function (){
@@ -17,10 +19,13 @@
 
 			var getMyRoutes = function (){
 				console.log('Getting your routes.')
+				return $http.get(HEROKU.URL + 'routes', HEROKU.CONFIG,
+					)
 			}
 
 			var getLocalRoutes = function (){
 				console.log('Getting your Local routes.')
+				return $http.post(HEROKU.URL + 'routes/search/', HEROKU.CONFIG)
 			}
 
 			var makeNewRoute = function (routeObj){
@@ -30,6 +35,11 @@
 					console.log(res.routes)
 				})
 				console.log('routeObj');
+			}
+
+			var editRoute = function (id){
+				console.log('Editing Existing Route');
+				return $http.patch(HEROKU.URL + 'routes/' + id, HEROKU.CONFIG)
 			}
 
 			
